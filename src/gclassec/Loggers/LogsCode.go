132604873353b9aa9f39f07cada34f3deb35e3b1
Ankur Tiwari <Ankur Tiwari>
2017-02-10 14:13:21 +0530

    Change in Log Path

--- src/gclassec/Loggers/LogsCode.go
+++ src/gclassec/Loggers/LogsCode.go
@@ -4,11 +4,20 @@ import (
 	"io"
 	"os"
 	"log"
+	"runtime"
+	"fmt"
+	"strings"
 )
 
 var logCloser io.Closer
 func MyLogger() {
-	logFile, err := os.OpenFile("C:/Git/goclassec/src/gclassec/Logs/log.txt", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	filename := "LogsCode.go"
+       _, filePath, _, _ := runtime.Caller(0)
+       fmt.Println("CurrentFilePath:==",filePath)
+       ConfigFilePath :=(strings.Replace(filePath, filename, "log.txt", 1))
+       fmt.Println("ABSPATH:==",ConfigFilePath)
+
+	logFile, err := os.OpenFile(ConfigFilePath, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
 	if err != nil {
 		panic(err)
 	}
