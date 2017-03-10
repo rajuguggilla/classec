783ee714b3a20dcbe4f73947bbef0a4663b480c8
priya <priya.malamkar@atmecs.com>
2017-03-10 16:20:27 +0530

    Adding configuration struct for job

--- src/gclassec/structs/configurationstruct/jobconfig.go
+++ src/gclassec/structs/configurationstruct/jobconfig.go
@@ -6,4 +6,6 @@ type Configuration struct {
 	Interval int64		`json:"Interval"`
 	Timespec time.Duration	`json:"Timespec"`
         UpdateUsingAPI  int64	`json:"UpdateUsingAPI"`
+	DynamicInterval int64	`json:"DynamicInterval"`
+        DynamicTimespec time.Duration	`json:"DynamicTimespec"`
 }
