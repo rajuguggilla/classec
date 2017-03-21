5d28d80144b682bad104ce9a69a7708c7e0d1921
priya <priya.malamkar@atmecs.com>
2017-03-21 13:10:34 +0530

    Updated tagging code to include instance name

--- src/gclassec/structs/tagstruct/tags_struct.go
+++ src/gclassec/structs/tagstruct/tags_struct.go
@@ -2,6 +2,7 @@ package tagstruct
 
 type Providers struct {
 	InstanceId string `gorm:"column:InstanceId"`
+	InstanceName	string	`gorm:"column:InstanceName"`
 	Cloud string `gorm:"column:Cloud"`
 	Tagname string `gorm:"column:Tagname"`
 }
